3a73ffdbac9dc9e303dd89dff99453688c355c58
Jack Yeung <jyeung.se@gmail.com>
2018-06-19 06:54:21 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)
-  i = 0
-  while array.length < i
-    yield array[i]
-    puts array[i]
-    i += 1
-  end
+  # i = 0
+  # while array.length < i
+  #   yield array[i]
+  #   puts array[i]
+  #   i += 1
+  # end
 end
